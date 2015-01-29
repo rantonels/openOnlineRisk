a53815bc46384ebb89bfa955b6d36ce2cdc2044a
Riccardo Antonelli <riccardo.antonelli@hotmail.it>
2015-01-29 13:47:29 +0100

    starting to write engine...

--- engine/oorengine.py
+++ engine/oorengine.py
@@ -2,6 +2,12 @@
 
 import logging
 
+#logging configuration
+LOG_FNAME = 'engine.log'
+logging.basicConfig(format='[%(asctime)s] %(levelname)s:\t%(message)s', filename=LOG_FNAME, level=logging.DEBUG)
+
+
+
 #game stages
 LOBBY 		= 0	#collecting players, modifying options
 OPENING 	= 1	#game has started, positioning armies
@@ -10,15 +16,27 @@ FINISHED	= 3	#game is over, winner was determined
 
 class Game:
 	def __init__(self):
+		logging.info('creating new game')
 		self.stage = LOBBY
 		self.players = []
 		self.log = []
+		self.winner = None
 
 	def start_game(self):
 		if (self.stage != LOBBY):
-			print "trying to start an already started game. No action will be performed."
-	
+			logging.error("trying to start an already started game. No action will be performed.")
+		else:
+			#start game		
 
-print "starting oor engine..."
+	def adjudicate(self,winner):
+		if not (winner in self.players):
+			logging.error("trying to adjudicate match to player not playing in this game! No action will be performed")
+		else:
+			self.winner = winner
+		
 
+logging.info("starting oor engine...")
 
+testgame = Game()
+
+testgame.start_game()
